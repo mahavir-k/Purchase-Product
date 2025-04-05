6daec223dbfb10c846bd165f2507de2f04d23830
mahavir-k <mahaviryadav004@gmail.com>
2025-04-05 09:29:10 +0530

    purchase product

---
+++ src/main/java/com/purchase/product/PurchaseProductApplication.java
@@ -0,0 +1,13 @@
+package com.purchase.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseProductApplication.class, args);
+	}
+
+}
